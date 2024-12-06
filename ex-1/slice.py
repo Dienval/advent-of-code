27a945e39ef4f8e6a6ea8c35c9a9f540140c72de
Dienval <nick.boggs1993@gmail.com>
2024-12-06 15:30:04 -0500

    submitting solution files for day 1 exercise

---
+++ ex-1/slice.py
@@ -0,0 +1,34 @@
+import re
+
+def main():
+
+    list_1 = []
+    list_2 = []
+
+    with open("ex-1/input.txt") as fp:
+        for line in fp:
+
+        # Testing with first line of input.txt
+        # text = "80784   47731"
+    
+        # Split string, removes whitespace and gives ['80784', '47731'] output
+            split_text = str.split(line)
+            #print(f"{split_text}")
+
+            list_1.append(split_text[0])
+            #print(f"List 1 contents: {list_1}")
+            list_2.append(split_text[1])
+            #print(f"List 2 contents: {list_2}")
+
+            new_list_1 = list(map(int, list_1))
+            new_list_1.sort()
+            print(new_list_1)
+
+            new_list_2 = list(map(int, list_2))
+            new_list_2.sort()
+            print(new_list_2)
+
+        distance = sum(abs(new_list_1[i] - new_list_2[i]) for i in range(len(new_list_1)))
+        print(f"Total distance between coordinates: {distance}")
+
+main()
